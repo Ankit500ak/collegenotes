d45809a712bb00f84c02820ffeb14007fb6429d7
unknown <ankit200211222@gmail.com>
2025-08-16 15:02:21 +0530

    m

--- collegenotes/collegenotes/settings.py
+++ collegenotes/collegenotes/settings.py
@@ -95,13 +95,11 @@ WSGI_APPLICATION = 'collegenotes.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Use dj-database-url for production database configuration
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
